e1c610c6c7a5d6f68dac838ce02f92ca11ed24db
thehatter <andrewlutenko@gmail.com>
2017-03-29 18:48:55 +0300

    mobile fiexes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-"use strict";
-var y = document.getElementsByClassName("menu-button");
-var menuButton = y[0];
-console.log(menuButton);
-var body       = document.body;
+$( document ).ready(function() {
+  
+  var menuButton = $('.menu-button');
+  var body       = $('body');
 
-menuButton.addEventListener('click', function() {
-  body.classList.toggle('menuActive');
-  this.classList.toggle('active');
+  $('.menu-button').click(function() {
+    $('body').toggleClass("menuActive");
+    $('.menu-button').toggleClass("menuActive");
+  });
+
 });
